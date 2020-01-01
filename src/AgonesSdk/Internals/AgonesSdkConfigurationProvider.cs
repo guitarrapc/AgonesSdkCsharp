23e0663f5e6ebef317e5eab55be6c7e71eb4e71f
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-01 23:50:57 +0900

    feat: service wip

---
+++ src/AgonesSdk/Internals/AgonesSdkConfigurationProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgonesSdk.Internals
+{
+    internal class AgonesSdkConfigurationProvider : ConfigurationProvider
+    {
+        public AgonesSdkSettings Settings {get;}
+
+        public AgonesSdkConfigurationProvider(AgonesSdkSettings settings)
+        {
+            Settings = settings;
+        }
+    }
+}
---
+++ src/AgonesSdk/Internals/AgonesSdkConfigurationSource.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgonesSdk.Internals
+{
+    internal class AgonesSdkConfigurationSource : IConfigurationSource
+    {
+        public AgonesSdkSettings Settings { get; }
+        public AgonesSdkConfigurationSource(AgonesSdkSettings settings)
+        {
+            Settings = settings;
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new AgonesSdkConfigurationProvider(Settings);
+        }
+    }
+}
