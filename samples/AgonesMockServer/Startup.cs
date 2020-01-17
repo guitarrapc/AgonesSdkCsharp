5f1dba2c34008244a0dc47412a6b2789f4322865
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-17 16:32:57 +0900

    chore: Add Mock Server

---
+++ samples/AgonesMockServer/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+
+namespace AgonesMockServer
+{
+    public class Startup
+    {
+        private static readonly byte[] nullResponse = new UTF8Encoding(false).GetBytes("{}");
+        private static readonly Func<HttpContext, HealthReport, Task> responseWriter = async (context, report) => await context.Response.BodyWriter.WriteAsync(nullResponse);
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddHealthChecks();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            
+            // dummy response
+            app.UseHealthChecks("/health", new HealthCheckOptions { ResponseWriter = responseWriter });
+            app.UseHealthChecks("/ready", new HealthCheckOptions { ResponseWriter = responseWriter });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
